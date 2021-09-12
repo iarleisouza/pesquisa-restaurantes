6ffc9c6055f708ffc06f4444f47a60b539b2630f
iarleisouza <iarlei.souza@gmail.com>
2021-09-12 18:15:45 -0300

    subindo projeto

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+//background-color: black;
+//background-color: ${(propos) => propos.theme.colors.primary}
+
+`;
