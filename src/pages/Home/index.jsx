86b17d89275a53f14525839c8e265c0bfb6383a0
iarleisouza <iarlei.souza@gmail.com>
2021-09-13 01:22:28 -0300

    criado Map para área do mapa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,22 +3,25 @@ import TextField, { Input} from '@material/react-text-field';
 import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
-import { Container, Search } from './styles';
+import { Container, Search, Logo, Wrapper, Map } from './styles';
 
 const Home = () =>  {
     const [inputValue, setInputValue] = useState('');
 return (
+    <Wrapper>
 <Container>
     <Search>
-        <img src={logo} alt="Logo do restaurante" />
+        <Logo src={logo} alt="Logo do restaurante" />
     <TextField
-          label='Pesquisar'
+          label='Pesquisar Restaurantes'
           outlined
          trailingIcon={<MaterialIcon role="button" icon="search" />}>
             <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
         </TextField>
         </Search>
  </Container>
+ <Map />
+ </Wrapper>
 );
 };
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,12 @@
-import React from 'react';
+
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+display: flex;
+flex-direction: row;
+
+
+`;
 
 export const Container = styled.aside`
 //background-color: black;
@@ -16,6 +23,17 @@ flex-direction: column;
 justify-content: center;
 background: #fff;
 padding: 16px;
+`;
 
+export const Logo = styled.img`
+width: 200px;
+margin: 0 0 15px 50px;
 
 `;
+
+export const Map = styled.div`
+
+background: red;
+width: 500px
+`;
+
